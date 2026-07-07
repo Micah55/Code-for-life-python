0c43ed4a04e6514ab922d6e918a291496d5ceb0e
Micah55 <micahevans1234@gmail.com>
2026-07-07 16:08:47 -0400

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+print("Hello world")
+print(9)
+print(True)
+x = 48
+print(x)
+type(True)
+
+
+
---
+++ input.py
@@ -0,0 +1,7 @@
+#name = input("Please enter your name: ")
+#print("Hello my name is " + name)
+#print(5 + 3)
+#print(20/ 2)
+#x = 10
+x = int(input("Please choose a number: "))
+print((5 * 5 ) * x)
