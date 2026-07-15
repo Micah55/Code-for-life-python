90a1097f017dbd3cb2852b62ee40c6cbcb6c5057
Micah55 <micahevans1234@gmail.com>
2026-07-15 16:26:02 -0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,45 @@
+import random
+#If statements
+#functions
+#inputs
+#Strings
+#loops
+#consequence
+#Use madlibs to help
+
+
+#def main(word,correctlttr1,correctlttr2,correctlttr3,correctlttr4,correctlttr5,correctlttr6,correctlttr7,correctlttr8):
+    #word = "Birthday"
+    #correctlttr1 = "B"
+    #correctlttr2 = "i"
+    #correctlttr3 = "r"
+    #correctlttr4 = "t"
+    #correctlttr5 = "h"
+    #correctlttr6 = "d"
+    #correctlttr7 = "a"
+    #correctlttr8 = "y"
+
+
+
+
+
+
+print("Welcome to hangman we will have your word shortly")
+words = ['Birthday','Restaurant','Sevant','Murderer']
+random_word = random.choice([words])
+#def choice(seq):
+    
+Letter = input("Please enter a letter")
+list(random_word)
+#if Letter == random_word: ind
+in
+
+
+
+
+
+    
+
+
+
+
